dfc1475a079603f029ad153cec3d5064e8962e30
siddharth <siddharthsundriyal08@gmail.com>
2022-09-17 23:21:27 +0530

    changed functionality in multiply.java

--- multiply.java
+++ multiply.java
@@ -2,7 +2,7 @@ class sid{
     public static void main(String[] args){
         int a = 10;
         int b = 20;
-        int sum = a+b;
-        system.out.println(sum+'yes please') 
+        int mul = a*b;
+        system.out.println(mul+'yes please') 
     }
 }
